008652421598b2728692d8330f3245f5b8244774
Marcos Chaves <marcosanf.c@gmail.com>
2023-10-30 08:31:01 -0300

    style(core): add colors and spaces

---
+++ lib/core/app/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class HColors {
+  static const primaryColor = Color(0xffd6a304);
+}
---
+++ lib/core/app/spaces.dart
@@ -0,0 +1,7 @@
+class HSpaces {
+  static const nano = 4.0;
+  static const xxs = 8.0;
+  static const xs = 16.0;
+  static const sm = 24.0;
+  static const md = 32.0;
+}
