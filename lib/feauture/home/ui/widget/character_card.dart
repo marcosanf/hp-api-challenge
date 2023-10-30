39ead94989275323eb7a844223d030fc868d5894
Marcos Chaves <marcosanf.c@gmail.com>
2023-10-30 08:34:21 -0300

    refactor(home): extract reusable card

---
+++ lib/feauture/home/ui/widget/character_card.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:hp_api_challenge/core/app/spaces.dart';
+
+class CharacterCard extends StatelessWidget {
+  final String? title;
+  final String? subtitle;
+  final String? imagepath;
+
+  const CharacterCard({
+    super.key,
+    this.title,
+    this.subtitle,
+    this.imagepath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const cardHeight = 450.0;
+    const cardWidth = 250.0;
+    return Center(
+      child: Container(
+        height: cardHeight,
+        width: cardWidth,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(HSpaces.sm),
+          color: Colors.white,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 5,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(HSpaces.sm),
+                ),
+                child: Image.network(
+                  imagepath ?? "",
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: HSpaces.xs,
+                  vertical: HSpaces.xxs,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(title ?? ""),
+                    Text(subtitle ?? ""),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
